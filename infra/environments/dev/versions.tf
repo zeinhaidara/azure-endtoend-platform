1aa365e8c562b72d164f14cc85638750505f5999
moulaye.haidara techconsulting.tech <moulaye.haidara@techconsulting.tech>
2026-09-14 17:43:14 +0000

    Merged PR 174: Feature/dev

--- infra/environments/dev/versions.tf
+++ infra/environments/dev/versions.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id    = var.subscription_id
+  subscription_id     = var.subscription_id
   storage_use_azuread = true
 }
---
+++ infra/modules/private-endpoints/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_private_endpoint" "storage" {
+  name                = "${var.name}-storage-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-storage-connection"
+    private_connection_resource_id = var.storage_account_id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name                 = "blob-dns-zone-group"
+    private_dns_zone_ids = [var.storage_dns_zone_id]
+  }
+}
+
+resource "azurerm_private_endpoint" "key_vault" {
+  name                = "${var.name}-keyvault-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-keyvault-connection"
+    private_connection_resource_id = var.key_vault_id
+    is_manual_connection           = false
+    subresource_names              = ["vault"]
+  }
+
+  private_dns_zone_group {
+    name                 = "vault-dns-zone-group"
+    private_dns_zone_ids = [var.key_vault_dns_zone_id]
+  }
+}
