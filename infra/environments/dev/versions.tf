076a9f55cec99d8d2e3483177b8e763d1197d007
Zein <moulaye.haidara@techconsulting.tech>
2026-09-14 09:12:16 -0400

    Added storage_use_azuread = true

--- infra/environments/dev/versions.tf
+++ infra/environments/dev/versions.tf
@@ -9,5 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id    = var.subscription_id
+  storage_use_azuread = true
 }
-
--- infra/environments/prod/versions.tf
+++ infra/environments/prod/versions.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  subscription_id    = var.subscription_id
+  storage_use_azuread = true
 }
-
