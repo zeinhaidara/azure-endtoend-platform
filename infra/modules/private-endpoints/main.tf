f055e03509114aa27f413bbf8007469359cc062b
Zein <moulaye.haidara@techconsulting.tech>
2026-09-14 14:35:51 -0400

    Add Dev SQL database and private connectivity

--- infra/modules/private-endpoints/main.tf
+++ infra/modules/private-endpoints/main.tf
@@ -35,3 +35,22 @@ resource "azurerm_private_endpoint" "key_vault" {
     private_dns_zone_ids = [var.key_vault_dns_zone_id]
   }
 }
+
+resource "azurerm_private_endpoint" "sql" {
+  name                = "${var.name}-sql-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-sql-connection"
+    private_connection_resource_id = var.sql_server_id
+    is_manual_connection           = false
+    subresource_names              = ["sqlServer"]
+  }
+
+  private_dns_zone_group {
+    name                 = "sql-dns-zone-group"
+    private_dns_zone_ids = [var.sql_dns_zone_id]
+  }
+}
